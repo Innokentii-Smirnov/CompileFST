585bae04c38e141e71ff3b894152057c93d4e612
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-12-31 21:40:10 +0300

    Initial commit

---
+++ CompileFST.sh
@@ -0,0 +1,9 @@
+filepath="$1"
+original_directory=$(pwd)
+directory=$(dirname "$filepath")
+name=$(basename "$filepath" .foma)
+binary="$name.bin"
+cd "$directory"
+foma -e "source $name.foma" -e "push $name" -e "save stack $binary" -e "sigma" -e "exit"
+echo "Created Foma binary $binary."
+cd "$original_directory"
